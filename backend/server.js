d880c3e77d823a324207dd296eb19fc8f705ff9c
Ghyslain BRUNO <ghyslain.bruno@orange.com>
2018-09-18 12:24:12 +0200

    File arrengements for Docker image

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require("../lazyker-568c4-firebase-adminsdk-b7xs7-03f3744551");
+const serviceAccount = require("./lazyker-568c4-firebase-adminsdk-b7xs7-03f3744551");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://lazyker-568c4.firebaseio.com"

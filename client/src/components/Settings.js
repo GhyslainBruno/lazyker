3e306c205076e170cf2f488d6864e71fd13c3cdb
Ghyslain BRUNO <ghyslain.bruno@orange.com>
2018-11-14 22:36:43 +0100

    Using https inside credentials files

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -450,7 +450,7 @@ class Settings extends Component {
     realdebridConnect = async () => {
         try {
 
-            let response = await fetch('https://api.real-debrid.com/oauth/v2/auth?client_id=GPA2MB33HLS3I&redirect_uri=http%3A%2F%2Fdedibox.ghyslain.xyz/api/link_rd&response_type=code&state=foo', {
+            let response = await fetch('https://api.real-debrid.com/oauth/v2/auth?client_id=GPA2MB33HLS3I&redirect_uri=https%3A%2F%2Fdedibox.ghyslain.xyz/api/link_rd&response_type=code&state=foo', {
                 method: 'GET',
                 headers: {
                     'Accept': 'application/json',
@@ -480,7 +480,7 @@ class Settings extends Component {
 
     render() {
 
-        const redirectUri = 'https://api.real-debrid.com/oauth/v2/auth?client_id=GPA2MB33HLS3I&redirect_uri=http%3A%2F%2Fdedibox.ghyslain.xyz/api/link_rd&response_type=code&state=foobar';
+        const redirectUri = 'https://api.real-debrid.com/oauth/v2/auth?client_id=GPA2MB33HLS3I&redirect_uri=https%3A%2F%2Fdedibox.ghyslain.xyz/api/link_rd&response_type=code&state=foobar';
 
         return (
             <div style={{width: '100%'}}>

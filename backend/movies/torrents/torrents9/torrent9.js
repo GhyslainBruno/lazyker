2104d8be3da43baf3cb060f9bff30ff7eb4d55a3
Ghyslain BRUNO <ghyslain.bruno@orange.com>
2018-11-27 17:11:37 +0100

    Torrent9 scraper modifications

--- backend/movies/torrents/torrents9/torrent9.js
+++ backend/movies/torrents/torrents9/torrent9.js
@@ -1,7 +1,7 @@
 const cheerio = require('cheerio');
 const rp  = require('request-promise');
 const logger = require('../../../logs/logger');
-const Torrent9RootUrl = 'https://ww1.torrent9.ph';
+const Torrent9RootUrl = 'https://www.torrent9.ch';
 const realdebrid = require('../../../realdebrid/debrid_links');
 
 /**
@@ -39,10 +39,10 @@ const getTorrentsList = async title => {
     try {
         const options = {
             method: 'POST',
-            uri: torrent9RealUrl + '/search_torrent/',
+            uri: torrent9RealUrl + '/recherche/' + title,
             json: true,
             formData: {
-                "champ_recherche" : title
+                "torrentSearch" : title
             },
             followAllRedirects: true
         };
@@ -50,24 +50,32 @@ const getTorrentsList = async title => {
         const response = await rp(options);
         const $ = cheerio.load(response);
 
-        const items = $('tr');
+        const items = $('.table-responsive > .table > tbody > tr');
 
         const torrentsList = [];
 
         items.map((i, result)  => {
 
-            const element = cheerio.load(result);
+            torrentsList.push({
+                title: result.children[1].children[2].attribs.title.replace('Télécharger ', '').replace(' en Torrent', ''),
+                url: result.children[1].children[2].attribs.href,
+                size: result.children[3].children[0].data,
+                seed: result.children[5].children[0].children[0].data,
+                leech: result.children[7].children[0].data
+            })
 
-            // Return elements which are only movies
-            if (element('i')[0].attribs.class === 'fa fa-video-camera') {
-                torrentsList.push({
-                    title: element('a')[0].attribs.title.replace('Télécharger ', '').replace(' en Torrent', ''),
-                    url: torrent9RealUrl + element('a')[0].attribs.href,
-                    size: element('td')[1].children[0].data,
-                    seed: element('td')[2].children[0].children[0].data,
-                    leech: element('td')[3].children[0].data
-                })
-            }
+            // const element = cheerio.load(result);
+            //
+            // // Return elements which are only movies
+            // if (element('i')[0].attribs.class === 'fa fa-video-camera') {
+            //     torrentsList.push({
+            //         title: element('a')[0].attribs.title.replace('Télécharger ', '').replace(' en Torrent', ''),
+            //         url: torrent9RealUrl + element('a')[0].attribs.href,
+            //         size: element('td')[1].children[0].data,
+            //         seed: element('td')[2].children[0].children[0].data,
+            //         leech: element('td')[3].children[0].data
+            //     })
+            // }
 
         });
 
@@ -88,9 +96,11 @@ const downloadTorrentFile = async (url, user) => {
 
     try {
 
+        const torrent9RealBaseUrl = await getRealUrl();
+
         const options = {
             method: 'GET',
-            uri: url,
+            uri: torrent9RealBaseUrl + url,
             json: true,
             followAllRedirects: true
         };

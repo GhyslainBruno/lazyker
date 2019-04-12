bb475b626e3867e3ef4fe0eb29efc84fae3f96cb
Ghyslain BRUNO <ghyslainbruno@gmail.com>
2019-04-12 17:21:40 +0200

    Trying to make a full CI/CD pipe on odroid c2

--- deploy.sh
+++ deploy.sh
@@ -19,31 +19,31 @@ if [ ! "$(docker ps -q -f name=lazyker)" ]; then
      docker run -p 8080:80 -p 443:443 --restart unless-stopped -v /home/ghys/lazyker/backend/coverage:/lazyker/app/backend/coverage --name lazyker -d ghyslainbruno/lazyker
 fi
 
-# Running backend tests - editing report file -> in the running container
-docker exec -it lazyker /bin/sh -c "cd backend;npm test Cloudscrapper;pwd=`pwd` && sed -i -e \"s|\/lazyker\/app\/backend|\/root\/src|g\" coverage/lcov.info"
-
-# Run sonarqube analysis on backend part
-cd backend && \
-docker run -v $(pwd):/root/src --link sonarqube newtmitch/sonar-scanner sonar-scanner \
-  -Dsonar.projectKey=lazyker-back \
-  -Dsonar.projectName='Lazyker Back' \
-  -Dsonar.sources=. \
-  -Dsonar.tests=. \
-  -Dsonar.test.inclusions='**/*.test.js' \
-  -Dsonar.exclusions='**/node_modules/**,**/coverage/**,**/*.json/**,**/*.pem/**,**/*.xml/**' \
-  -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
-  -Dsonar.testExecutionReportPaths=coverage/test-report.xml \
-  -Dsonar.host.url='http://sonarqube.ghyslain.xyz:9000' \
-  -Dsonar.login=c37d841c739531680ebf8c1f874012806bd01da7 && \
-
-# Run sonarqube analysis on frontend part
-cd .. && \
-cd client && \
-docker run -v $(pwd):/root/src --link sonarqube newtmitch/sonar-scanner sonar-scanner \
-  -Dsonar.projectKey=lazyker-front \
-  -Dsonar.projectName='Lazyker Front' \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=http://sonarqube.ghyslain.xyz:9000 \
-  -Dsonar.login=c37d841c739531680ebf8c1f874012806bd01da7 && \
+## Running backend tests - editing report file -> in the running container
+#docker exec -it lazyker /bin/sh -c "cd backend;npm test Cloudscrapper;pwd=`pwd` && sed -i -e \"s|\/lazyker\/app\/backend|\/root\/src|g\" coverage/lcov.info"
+#
+## Run sonarqube analysis on backend part
+#cd backend && \
+#docker run -v $(pwd):/root/src --link sonarqube newtmitch/sonar-scanner sonar-scanner \
+#  -Dsonar.projectKey=lazyker-back \
+#  -Dsonar.projectName='Lazyker Back' \
+#  -Dsonar.sources=. \
+#  -Dsonar.tests=. \
+#  -Dsonar.test.inclusions='**/*.test.js' \
+#  -Dsonar.exclusions='**/node_modules/**,**/coverage/**,**/*.json/**,**/*.pem/**,**/*.xml/**' \
+#  -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
+#  -Dsonar.testExecutionReportPaths=coverage/test-report.xml \
+#  -Dsonar.host.url='http://sonarqube.ghyslain.xyz:9000' \
+#  -Dsonar.login=c37d841c739531680ebf8c1f874012806bd01da7 && \
+#
+## Run sonarqube analysis on frontend part
+#cd .. && \
+#cd client && \
+#docker run -v $(pwd):/root/src --link sonarqube newtmitch/sonar-scanner sonar-scanner \
+#  -Dsonar.projectKey=lazyker-front \
+#  -Dsonar.projectName='Lazyker Front' \
+#  -Dsonar.sources=. \
+#  -Dsonar.host.url=http://sonarqube.ghyslain.xyz:9000 \
+#  -Dsonar.login=c37d841c739531680ebf8c1f874012806bd01da7 && \
 
 cd /home/ghys/lazyker

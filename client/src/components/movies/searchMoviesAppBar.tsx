c5646940d6f7af5e7534e6cb0819532b9a19d009
Ghyslain Bruno <ghyslainbruno@gmail.com>
2021-06-04 23:15:36 +0200

    fix(ui error): You are providing an onClick event listener to a child of a button element.

--- client/src/components/movies/searchMoviesAppBar.tsx
+++ client/src/components/movies/searchMoviesAppBar.tsx
@@ -34,67 +34,72 @@ type SearchMoviesAppBarProps = {
 const SearchMoviesAppBar = (props: SearchMoviesAppBarProps) => {
   return (
 
-        // @ts-ignore
-        <div style={{flexGrow: '1'}}>
-            <AppBar
-                position="static"
-                color="default">
-                <Toolbar>
-                        <IconButton style={props.isInSearchView ? {visibility: 'visible', marginRight: '16px'} : {visibility: 'hidden', marginRight: '16px'}}>
-                        <ArrowBack onClick={props.cleanSearch}/>
-                    </IconButton>
+    // @ts-ignore
+    <div style={{flexGrow: '1'}}>
+      <AppBar
+        position="static"
+        color="default">
+        <Toolbar>
+          <IconButton
+            onClick={props.cleanSearch}
+            style={props.isInSearchView ? {visibility: 'visible', marginRight: '16px'} : {visibility: 'hidden', marginRight: '16px'}}>
+            <ArrowBack />
+          </IconButton>
 
-                    <Input
-                        id="movie_title_to_search"
-                        value={props.movieTitleToSearch}
-                        onClick={props.getMoviesGenres}
-                        placeholder="Movie title"
-                        autoComplete="off"
-                        // type="search"
-                        onChange={evt => props.updateMovieTitleToSearch(evt)}
-                        onBlur={evt => props.searchBarLostFocus(evt)}
-                        disableUnderline={true}
-                        style={{width: '80%'}}
-                        onKeyPress={(event) => {props.onEnterKeyPressed(event)}}
-                    />
-
-                    <IconButton>
-                        {props.movieTitleToSearch !== null && props.movieTitleToSearch !== '' ?
-                            <Close onClick={props.clearTitle}/>
-                            :
-                            // @ts-ignore
-                            <Search onClick={props.movieTitleToSearch !== null && props.movieTitleToSearch !== '' ? () => props.searchMovie : null}/>
-                        }
-                    </IconButton>
-
-                </Toolbar>
-            </AppBar>
-
-            <Paper elevation={1} style={props.movieGenres.length > 0 ? {padding: '5px'} : {display: 'none'}}>
-
-                <CircularProgress style={props.movieGenresLoading ? {display: 'inline-block'} : {display: 'none'}}/>
-
-                {
-                    props.movieGenres !== null ? props.movieGenres.map(movieGenre => {
-                            return (
-                                <Chip label={movieGenre.name} style={props.styles.outlinedChip} className="movieGenre" clickable={true} onClick={() => props.searchMovieGenre(movieGenre)} />
-                            )
-                        })
-                        :
-                        null
-                }
-
-            </Paper>
-
-            <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
-                <Chip label="Now Playing" style={props.moviesGenre.name === 'Now Playing' ? props.styles.selectedChip : props.styles.outlinedChip} clickable={true} onClick={() => props.searchMovieGenre({name: "Now Playing", id: "now_playing"})}/>
-                <Chip label="Popular" style={props.moviesGenre.name === 'Popular' ? props.styles.selectedChip : props.styles.outlinedChip} clickable={true} onClick={() => props.searchMovieGenre({name: "Popular", id: "popular"})} />
-                <Chip label="Top Rated" style={props.moviesGenre.name === 'Top Rated' ? props.styles.selectedChip : props.styles.outlinedChip} clickable={true} onClick={() => props.searchMovieGenre({name: "Top Rated", id: "top_rated"})} />
-                <Chip label="Upcoming" style={props.moviesGenre.name === 'Upcoming' ? props.styles.selectedChip : props.styles.outlinedChip} clickable={true} onClick={() => props.searchMovieGenre({name: "Upcoming", id: "upcoming"})} />
-            </div>
+          <Input
+            id="movie_title_to_search"
+            value={props.movieTitleToSearch}
+            onClick={props.getMoviesGenres}
+            placeholder="Movie title"
+            autoComplete="off"
+            // type="search"
+            onChange={evt => props.updateMovieTitleToSearch(evt)}
+            onBlur={evt => props.searchBarLostFocus(evt)}
+            disableUnderline={true}
+            style={{width: '80%'}}
+            onKeyPress={(event) => {props.onEnterKeyPressed(event)}}
+          />
 
 
-        </div>
-    )
+          {props.movieTitleToSearch !== null && props.movieTitleToSearch !== '' ?
+            <IconButton>
+              <Close onClick={props.clearTitle}/>
+            </IconButton>
+            :
+            <IconButton>
+              {/*// @ts-ignore*/}
+              <Search onClick={props.movieTitleToSearch !== null && props.movieTitleToSearch !== '' ? () => props.searchMovie : null}/>
+            </IconButton>
+          }
+
+        </Toolbar>
+      </AppBar>
+
+      <Paper elevation={1} style={props.movieGenres.length > 0 ? {padding: '5px'} : {display: 'none'}}>
+
+        <CircularProgress style={props.movieGenresLoading ? {display: 'inline-block'} : {display: 'none'}}/>
+
+        {
+          props.movieGenres !== null ? props.movieGenres.map(movieGenre => {
+              return (
+                <Chip label={movieGenre.name} style={props.styles.outlinedChip} className="movieGenre" clickable={true} onClick={() => props.searchMovieGenre(movieGenre)} />
+              )
+            })
+            :
+            null
+        }
+
+      </Paper>
+
+      <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
+        <Chip label="Now Playing" style={props.moviesGenre.name === 'Now Playing' ? props.styles.selectedChip : props.styles.outlinedChip} clickable={true} onClick={() => props.searchMovieGenre({name: "Now Playing", id: "now_playing"})}/>
+        <Chip label="Popular" style={props.moviesGenre.name === 'Popular' ? props.styles.selectedChip : props.styles.outlinedChip} clickable={true} onClick={() => props.searchMovieGenre({name: "Popular", id: "popular"})} />
+        <Chip label="Top Rated" style={props.moviesGenre.name === 'Top Rated' ? props.styles.selectedChip : props.styles.outlinedChip} clickable={true} onClick={() => props.searchMovieGenre({name: "Top Rated", id: "top_rated"})} />
+        <Chip label="Upcoming" style={props.moviesGenre.name === 'Upcoming' ? props.styles.selectedChip : props.styles.outlinedChip} clickable={true} onClick={() => props.searchMovieGenre({name: "Upcoming", id: "upcoming"})} />
+      </div>
+
+
+    </div>
+  )
 };
 export default SearchMoviesAppBar
